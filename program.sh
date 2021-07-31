bc3d7e25ee266c8eaabebe247dd93907709d3ed4
sportleafs-ui <sportleafs-ui@program>
2021-08-01 01:19:42 +0300

    i remove no-hitory push from program.sh

--- program.sh
+++ program.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-push(){
-  ORIGIN=$(git remote get-url origin)
-  rm -rf .git
-  git init -b main
-  git remote add origin $ORIGIN
-  git config --local include.path ../.gitconfig
-  git add .
-  git commit -m "i am sportleafs-ui program"
-  git push -f -u origin main
+repl(){
+  clj -A:repl
 }
 
 "$@"
