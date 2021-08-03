d27a611e903893747f4b452f4f97db168509371f
sportleafs-ui <sportleafs-ui@program>
2021-08-03 16:58:12 +0300

    i use deps-repl library

--- program.sh
+++ program.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 repl(){
-  clj -A:repl
+  clj \
+    -X:repl deps-repl.core/process \
+    :main-ns sportleafs-ui.main \
+    :port 7788 \
+    :host '"0.0.0.0"' \
+    :repl? true \
+    :nrepl? false
 }
 
 "$@"
