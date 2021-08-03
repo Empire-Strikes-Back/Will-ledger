b19454e4de06a05931ed5609042a85c4c75dda97
sportleafs-ui <sportleafs-ui@program>
2021-08-03 20:01:10 +0300

    i revert "i use deps-repl library"
    This reverts commit d27a611e903893747f4b452f4f97db168509371f.

--- program.sh
+++ program.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 repl(){
-  clj \
-    -X:repl deps-repl.core/process \
-    :main-ns sportleafs-ui.main \
-    :port 7788 \
-    :host '"0.0.0.0"' \
-    :repl? true \
-    :nrepl? false
+  clj -A:repl
 }
 
 "$@"
